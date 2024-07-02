d4e47fb6d70724967f3bca539588864b222eb0a5
Thel_Mymy30 <161165028+nthelma30@users.noreply.github.com>
2024-07-02 14:13:41 +0100

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById('myButton').addEventListener('click', () => {
+        document.getElementById('message').textContent = 'Hello, welcome to my DevOps engineer portfolio!';
+    });
+});
